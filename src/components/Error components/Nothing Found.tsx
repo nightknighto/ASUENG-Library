4ff76edc5950e65770a227b12189181d391f1c2c
Deadreyo <ahmedatwa866@yahoo.com>
2022-01-06 23:28:48 +0200

    Updates

--- src/components/Error components/Nothing Found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NothingFound() {
-
-    return (
-        <div className="ms-2 mt-3">
-            <i className="fas fa-question-circle fa-5x"></i>
-            <p className="fs-5">Nothing Found</p>
-        </div>
-    )
-}
